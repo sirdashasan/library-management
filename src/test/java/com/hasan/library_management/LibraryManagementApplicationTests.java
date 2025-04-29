e410eb676488116d4b254d6066837910904eed31
sirdashasan <sirdashasan@hotmail.com>
2025-04-29 10:54:21 +0300

    Initialize project setup: Spring Boot 3, Java 21, PostgreSQL configuration

---
+++ src/test/java/com/hasan/library_management/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasan.library_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
