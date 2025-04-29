5594d3185b78d8f3cde0caadebe8096ee0b3e45a
sirdashasan <sirdashasan@hotmail.com>
2025-04-29 15:40:43 +0300

    Add BookMapper

---
+++ src/main/java/com/hasan/library_management/mapper/BookMapper.java
@@ -0,0 +1,39 @@
+package com.hasan.library_management.mapper;
+
+import com.hasan.library_management.dto.request.BookRequestDto;
+import com.hasan.library_management.dto.response.BookResponseDto;
+import com.hasan.library_management.entity.Book;
+
+public class BookMapper {
+
+    public static Book toEntity(BookRequestDto dto) {
+        return Book.builder()
+                .title(dto.getTitle())
+                .author(dto.getAuthor())
+                .isbn(dto.getIsbn())
+                .publicationDate(dto.getPublicationDate())
+                .genre(dto.getGenre())
+                .available(true)
+                .build();
+    }
+
+    public static BookResponseDto toResponseDto(Book book) {
+        return BookResponseDto.builder()
+                .id(book.getId())
+                .title(book.getTitle())
+                .author(book.getAuthor())
+                .isbn(book.getIsbn())
+                .publicationDate(book.getPublicationDate())
+                .genre(book.getGenre())
+                .available(book.isAvailable())
+                .build();
+    }
+
+    public static void updateEntity(Book book, BookRequestDto dto) {
+        book.setTitle(dto.getTitle());
+        book.setAuthor(dto.getAuthor());
+        book.setIsbn(dto.getIsbn());
+        book.setPublicationDate(dto.getPublicationDate());
+        book.setGenre(dto.getGenre());
+    }
+}
