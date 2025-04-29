1d6d5726da482e65bb69e0283394e92bcfb934ea
sirdashasan <sirdashasan@hotmail.com>
2025-04-29 11:16:23 +0300

    Add User entity

---
+++ src/main/java/com/hasan/library_management/entity/User.java
@@ -0,0 +1,34 @@
+package com.hasan.library_management.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
